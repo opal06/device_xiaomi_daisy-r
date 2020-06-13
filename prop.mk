f0e9e003ba9cc3c9042cbe317be61369fd557a7a
a-huk <56552991+a-huk@users.noreply.github.com>
2020-06-13 21:00:36 +0200

    More SurfaceFlinger props

--- prop.mk
+++ prop.mk
@@ -305,16 +305,30 @@ persist.sys.fflag.override.settings_network_and_internet_v2=true
 
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+ro.surface_flinger.protected_contents=true \
 ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
 ro.surface_flinger.max_virtual_display_dimension=4096 \
-ro.surface_flinger.protected_contents=true \
+ro.surface_flinger.max_virtual_display_dimension=4096 \
 ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
 ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
-ro.surface_flinger.use_color_management=true \
-debug.sf.early_gl_app_phase_offset_ns=15000000 \
-ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-ro.surface_flinger.max_virtual_display_dimension=4096
+ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 
+PRODUCT_PROPERTY_OVERRIDES += \
+debug.sf.early_phase_offset_ns=1500000 \
+debug.sf.early_app_phase_offset_ns=1500000 \
+debug.sf.early_gl_phase_offset_ns=3000000 \
+    debug.sf.early_gl_app_phase_offset_ns=15000000 \
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.disable_backpressure=1 \
+    debug.sf.enable_gl_backpressure=1
+
+# The default sf phase offset is set to 6ms, to avoid it be included into next
+# vsync threshold, set high fps early sf and next vsync threshold phase offset
+# to 6.1ms, which is bigger than all sf phase offsets in normal frame rate.
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.high_fps_early_phase_offset_ns=6100000 \
+    debug.sf.high_fps_early_gl_phase_offset_ns=9000000 \
+    debug.sf.phase_offset_threshold_for_next_vsync_ns=6100000
 # Thermal configs path
 PRODUCT_PROPERTY_OVERRIDES += \
 sys.thermal.data.path=/data/vendor/thermal/
