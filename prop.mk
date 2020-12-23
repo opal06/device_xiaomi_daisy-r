bb83cfa2f3e33a008422fb422ef3009a981ada67
a-huk <56552991+a-huk@users.noreply.github.com>
2020-12-23 11:15:27 +0100

    Lineageify

--- prop.mk
+++ prop.mk
@@ -200,9 +200,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ro.sys.fw.dex2oat_thread_count=8 \
 ro.vendor.extension_library=libqti-perfd-client.so
 
-# Display cutout
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.potato.has_cutout=true
 
 # Perfetto
 PRODUCT_PROPERTY_OVERRIDES += \
